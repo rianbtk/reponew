1ba5a5c55438cb8d9db34c1a2782a3fc1fb0c7c8
rianeva <128794539+rianeva@users.noreply.github.com>
2024-08-15 08:47:46 +0700

    done

---
+++ wals/public/python-resources/unittest/jawaban/66bd5590a1519_1723684240.py
@@ -0,0 +1,15 @@
+# Tuliskan kode program dibawah ini
+tulis = "Belajar Python"
+
+def kapital(tulis):
+    #Tuliskan kode program dibawah ini
+    return tulis.upper()
+print(kapital(tulis))
+9.
+def hitung_luas_segitiga():
+  alas = 5
+  tinggi = 7
+  luas = (alas * tinggi) / 2
+  print("Luas Segitiga adalah: ", luas) 
+
+hitung_luas_segitiga()
